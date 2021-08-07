5d3421e907d3b7f5c8582c86e19476f7d88d515e
埃拉拉 <ElaBosak233@163.com>
2021-08-07 12:09:38 +0800

    move

--- app.js
+++ app.js
@@ -12,24 +12,24 @@ console.log(`
                        /____/                          
         Powered by @ElaBosak233, Made with L♥VE
          https://github.com/ElaBosak233/eco.git
-`)
+`);
 
-require('json5/lib/register');
+require("json5/lib/register");
 const config = require("./eco.config.json5");
 const exec = require("child_process").exec;
 
-const vite = exec("vite --port " + config.port.eco_vite, {});
+const vite = exec("vite ./view --port " + config.port.eco_vite, {});
 vite.stdout.on("data", function(data) {
     console.log("[·EcoVite]\n" + data);
-})
+});
 vite.stderr.on("data", function(data) {
     console.error("[×EcoVite]\n" + data);
-})
+});
 
 const express = exec("node ./api/index.js", {});
 express.stdout.on("data", function(data) {
     console.log("[·EcoExpress]\n" + data);
-})
+});
 express.stderr.on("data", function(data) {
     console.error("[×EcoExpress]\n" + data);
-})
+});
